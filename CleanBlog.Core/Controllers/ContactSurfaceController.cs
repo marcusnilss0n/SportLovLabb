8df6e3a6a2213bee03ef64b4646a5905bf6bd960
marcusnilss0n <nilsson.marcus90@gmail.com>
2022-02-24 15:42:20 +0100

    Added contact form & fixed footer & article grid

---
+++ CleanBlog.Core/Controllers/ContactSurfaceController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Umbraco.Web.Mvc;
+using System.Web.Mvc;
+using CleanBlog.Core.ViewModels;
+using System.Web;
+using Umbraco.Web;
+using System.Net.Mail;
+using Umbraco.Core.Logging;
+using CleanBlog.Core.Services;
+
+namespace CleanBlog.Core.Controllers
+{
+    public class ContactSurfaceController  :  SurfaceController
+    {
+        private readonly ISmtpService _smtpService;
+        public ContactSurfaceController(ISmtpService smtpService)
+        {
+            _smtpService = smtpService; 
+        }
+
+        [HttpGet]
+        public ActionResult RenderForm()
+        {
+            ContactViewModel model = new ContactViewModel() { ContactFormId = CurrentPage.Id };
+            return PartialView("~/Views/Partials/Contact/contactForm.cshtml", model);
+        }
+
+        [HttpPost]
+        public ActionResult RenderForm(ContactViewModel model)
+        {
+            return PartialView("~/Views/Partials/Contact/contactForm.cshtml", model);
+        }
+
+        public ActionResult SubmitForm(ContactViewModel model)
+        {
+            bool success = false;
+
+            if (ModelState.IsValid)
+            {
+                success = _smtpService.SendEmail(model);
+            }
+
+            var contactPage = UmbracoContext.Content.GetById(false, model.ContactFormId);
+
+            var successMessage = contactPage.Value<IHtmlString>("successMessage");
+            var errorMessage = contactPage.Value<IHtmlString>("errorMessage");
+
+            return PartialView("~/Views/Partials/Contact/result.cshtml", success ? successMessage : errorMessage);
+        }
+
+
+    }
+}
